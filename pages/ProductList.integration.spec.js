eee6b33d1eb8a20ae595a3432ab5842e92f84af0
Felipe Ribeiro <piporibeiro@gmail.com>
2021-11-28 21:45:49 -0300

    test: mount the Search component as child

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -1,12 +1,18 @@
 import { mount } from '@vue/test-utils'
+import Search from '../components/Search'
 import ProductList from '.'
 // import ProductCard from '../components/ProductCard'
-// import Search from '../components/Search'
 
 describe('ProductList -integration', () => {
   it('should mount the component', () => {
     const wrapper = mount(ProductList)
 
     expect(wrapper.vm).toBeDefined()
+  })
+
+  it('should mount the Search component as child', () => {
+    const wrapper = mount(ProductList)
+
+    expect(wrapper.findComponent(Search)).toBeDefined()
   })
 })
