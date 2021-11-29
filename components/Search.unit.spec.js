3fed2703518668093fd8084bf0aacd3bd65be074
Felipe Ribeiro <piporibeiro@gmail.com>
2021-11-28 21:26:12 -0300

    test: emit search when search input is cleared

--- components/Search.unit.spec.js
+++ components/Search.unit.spec.js
@@ -7,7 +7,7 @@ describe('Search -unit', () => {
     expect(wrapper.vm).toBeDefined()
   })
 
-  it('should emit search expect when form is submitted', async () => {
+  it('should emit search when form is submitted', async () => {
     const wrapper = mount(Search)
     const term = 'termo para busca'
     await wrapper.find('input[type="search"]').setValue(term)
@@ -15,5 +15,18 @@ describe('Search -unit', () => {
     expect(wrapper.emitted().doSearch).toBeTruthy()
     expect(wrapper.emitted().doSearch.length).toBe(1)
     expect(wrapper.emitted().doSearch[0]).toEqual([{ term }])
+  })
+
+  it('should emit search when search input is cleared', async () => {
+    const wrapper = mount(Search)
+    const term = 'termo para busca'
+    const input = wrapper.find('input[type="search"]')
+
+    await input.setValue(term)
+    await input.setValue('')
+
+    expect(wrapper.emitted().doSearch).toBeTruthy()
+    expect(wrapper.emitted().doSearch.length).toBe(1)
+    expect(wrapper.emitted().doSearch[0]).toEqual([{ term: '' }])
   })
 })
