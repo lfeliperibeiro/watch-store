28d12cb49408012fd54e5cb07271669a83c7fe74
Felipe Ribeiro <piporibeiro@gmail.com>
2021-11-28 22:45:27 -0300

    test: display the error when Promise rejects

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -60,4 +60,18 @@ describe('ProductList -integration', () => {
 
     expect(products).toHaveLength(10)
   })
+
+  it('should display the error when Promise rejects', async () => {
+    axios.get.mockReturnValue(Promise.reject(new Error(' ')))
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    })
+
+    await Vue.nextTick()
+
+    expect(wrapper.text()).toContain('Problemas ao carregar a lista!')
+  })
 })
