71184908dce5739d26fa5ff87c9ab4888ee30d76
Felipe Ribeiro <piporibeiro@gmail.com>
2021-12-08 20:36:53 -0300

    feat: add cartState in product

--- layouts/default.vue
+++ layouts/default.vue
@@ -102,7 +102,7 @@
         </nav>
       </div>
     </header>
-    <cart :is-open="isCartOpen" @close="toggleCart" />
+    <cart :products="products" :is-open="isCartOpen" @close="toggleCart" />
     <nuxt />
     <footer class="bg-gray-200">
       <div
@@ -119,17 +119,21 @@
 
 <script>
 import Cart from '../components/Cart'
+import { cartState } from '../state'
 
 export default {
   components: { Cart },
-  data() {
-    return {
-      isCartOpen: false,
-    }
+  computed: {
+    isCartOpen() {
+      return cartState.open
+    },
+    products() {
+      return cartState.items
+    },
   },
   methods: {
     toggleCart() {
-      this.isCartOpen = !this.isCartOpen
+      cartState.open = !cartState.open
     },
   },
 }
