75a6fbe603cda622306afda6ff8f34ac8044695f
Felipe Ribeiro <piporibeiro@gmail.com>
2021-12-08 20:36:10 -0300

    test: add item to cartState on button click

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+import { cartState } from '../state'
 import ProductCard from './ProductCard'
 import { makeServer } from '@/miragejs/server'
 
@@ -41,13 +42,12 @@ describe('ProductCard -unit', () => {
     expect(wrapper.text()).toContain('25.00')
   })
 
-  it('should emit the event addToCart with product object when button get clicked', async () => {
-    const { wrapper, product } = mountProductCard(server)
-
+  it('should add item to cartState on button click', async () => {
+    const { wrapper } = mountProductCard(server)
     await wrapper.find('button').trigger('click')
 
-    expect(wrapper.emitted().addToCart).toBeTruthy()
-    expect(wrapper.emitted().addToCart.length).toBe(1)
-    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
+    expect(cartState.items).toHaveLength(1)
   })
+
+  it.todo('should ensure product is not added to the cart twice')
 })
