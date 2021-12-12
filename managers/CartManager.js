462d4ec71198b90c7b943841d9e6d58eacc738a8
Felipe Ribeiro <piporibeiro@gmail.com>
2021-12-12 19:09:52 -0300

    feat: return remove product from the cart

--- managers/CartManager.js
+++ managers/CartManager.js
@@ -35,4 +35,11 @@ export class CartManager {
 
     return this.state
   }
+
+  removeProduct(productId) {
+    this.state.items = [
+      ...this.state.items.filter((product) => product.id !== productId),
+    ]
+    return this.state
+  }
 }
