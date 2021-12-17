794808fe42a215f64aff12538518fd0db17ba7e2
Felipe Ribeiro <piporibeiro@gmail.com>
2021-12-17 17:47:55 -0300

    chore: configure eslint for cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended',
-    'prettier'
+    'prettier',
+    'plugin:cypress/recommended',
   ],
-  plugins: [
-  ],
+
+  plugins: ['cypress'],
+
   // add your custom rules here
-  rules: {}
+  rules: {
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-unnecessary-waiting': 'error',
+    'cypress/assertion-before-screenshot': 'warn',
+    'cypress/no-force': 'warn',
+    'cypress/no-async-tests': 'error',
+    'cypress/no-pause': 'error',
+  },
 }
---
+++ cypress/e2e/Store.spec.js
@@ -0,0 +1,18 @@
+import { makeServer } from '../../miragejs/server'
+
+context('Store', () => {
+  let server
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' })
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+  it('should display the store', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('body').contains('Brand')
+    cy.get('body').contains('Wrist Watch')
+  })
+})
