097f3d23e5e93979a8f35d1bfc4e8b9af926204a
Felipe Ribeiro <piporibeiro@gmail.com>
2021-12-17 20:12:41 -0300

    test: type in the search field

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -15,4 +15,27 @@ context('Store', () => {
     cy.get('body').contains('Brand')
     cy.get('body').contains('Wrist Watch')
   })
+
+  context('Store > Search for products', () => {
+    it('should type in the search field ', () => {
+      cy.visit('http://localhost:3000')
+      cy.get('input[type=search]')
+        .type('Some text here')
+        .should('have.value', 'Some text here')
+    })
+
+    it.only('should type in the search field ', () => {
+      server.create('product', {
+        title: 'relogio',
+      })
+      server.createList('product', 10)
+
+      cy.visit('http://localhost:3000')
+
+      cy.get('input[type="search"]').type('relogio')
+      cy.get('[data-testid="search-form"]').submit()
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1)
+    })
+  })
 })
