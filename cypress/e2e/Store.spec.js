cd751c127df10c5d23533c1eb326c2b257a5fca9
Felipe Ribeiro <piporibeiro@gmail.com>
2021-12-20 19:56:46 -0300

    test: Product List

--- cypress/e2e/Store.spec.js
+++ cypress/e2e/Store.spec.js
@@ -10,15 +10,37 @@ context('Store', () => {
     server.shutdown()
   })
   it('should display the store', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
 
     cy.get('body').contains('Brand')
     cy.get('body').contains('Wrist Watch')
   })
 
+  context('Store > Product List', () => {
+    it('should display "0 products" when no products is returned', () => {
+      cy.visit('/')
+      cy.get('[data-testid="product-card"]').should('have.length', 0)
+      cy.get('body').contains('0 products')
+    })
+
+    it('should display "1 product" when 1 product is returned', () => {
+      server.create('product')
+      cy.visit('/')
+      cy.get('[data-testid="product-card"]').should('have.length', 1)
+      cy.get('body').contains('1 product')
+    })
+
+    it('should display "10 products" when 10 products are returned', () => {
+      server.createList('product', 10)
+      cy.visit('/')
+      cy.get('[data-testid="product-card"]').should('have.length', 10)
+      cy.get('body').contains('10 products')
+    })
+  })
+
   context('Store > Search for products', () => {
     it('should type in the search field ', () => {
-      cy.visit('http://localhost:3000')
+      cy.visit('/')
       cy.get('input[type=search]')
         .type('Some text here')
         .should('have.value', 'Some text here')
@@ -30,7 +52,7 @@ context('Store', () => {
       })
       server.createList('product', 10)
 
-      cy.visit('http://localhost:3000')
+      cy.visit('/')
 
       cy.get('input[type="search"]').type('relogio')
       cy.get('[data-testid="search-form"]').submit()
@@ -42,7 +64,7 @@ context('Store', () => {
       server.create('product')
       server.createList('product', 10)
 
-      cy.visit('http://localhost:3000')
+      cy.visit('/')
 
       cy.get('input[type="search"]').type('relogio')
       cy.get('[data-testid="search-form"]').submit()
